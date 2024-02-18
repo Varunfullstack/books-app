5736a9d7cb021405416304dfd693623063c6f1c0
Varun Kamani <varunphp.dev@gmail.com>
2024-02-18 12:08:28 +0530

    doc added

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -1,6 +1,7 @@
 // BookContext.js
 import React from "react";
 
+// Provides a context for managing book sections, including functionalities to add, edit, and remove sections.
 const BookContext = React.createContext({
   sections: [],
   addSection: () => {},
